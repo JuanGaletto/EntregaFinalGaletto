7439af2f934b56eae2c0907bb27c6a872a8eafc8
Juan Galetto <jpgaletto@hotmail.com>
2024-05-31 17:17:53 -0300

    xd

--- src/components/Brand/Brand.jsx
+++ src/components/Brand/Brand.jsx
@@ -1,17 +1,18 @@
 function Brand() {
-  return (
-    <div>
-      <h1
-        style={{
-          color: "#006400",
-          fontSize: "40px",
-        }}
-      >
-        ANOKA SPORTS
-      </h1>
-    </div>
-  );
-}
+    return (
+      <div>
+        <h1
+          style={{
+            color: "#006400",
+            fontSize: "40px",
+          }}
+        >
+          ANOKA SPORTS
+        </h1>
+      </div>
+    );
+  }
+  
 
-
-export default Brand;
+  export default Brand;
+  
